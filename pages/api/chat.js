97591bac27d74df5c15f8a5f5232c51454607e27
Aaric <aaricchen@gmail.com>
2023-03-13 23:20:13 +0800

    update chat api

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -6,14 +6,11 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const messages = req.body.messages;
-  console.log(messages);
   if (messages) {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{ role: "system", content: "You are a helpful assistant." }, ...messages],
     });
-    console.log(completion);
-    console.log(completion.data.choices);
     if (completion.data.choices) {
       res.status(200).json({ result: completion.data.choices[0].message.content });
     } else {
